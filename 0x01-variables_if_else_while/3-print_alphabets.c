a14adf4184af3b55ce6f1600c4f99c43dd2e7584
nickonyino <bahatiluiz@gmail.com>
2023-07-14 07:01:17 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - A program that prints alphabet
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
