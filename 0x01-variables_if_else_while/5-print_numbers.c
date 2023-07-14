de93f5f5b917ea89167b96d70e788b0abb00e9d2
nickonyino <bahatiluiz@gmail.com>
2023-07-14 10:39:02 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description - print numbers of base 10
+ * Description - 'print numbers of base 10'
  * Return: Always 0
  */
 int main(void)
-
 {
 	int n;
 
