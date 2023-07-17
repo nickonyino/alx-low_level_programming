e9ad99ad32a01670cdd3d8031da39921228cb1b0
nickonyino <bahatiluiz@gmail.com>
2023-07-17 05:47:20 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,12 @@ int main(void)
 	for (n =48; n <= 57; n++)
 	{
 		putchar(n);
-			if (n == 57)
+		if (n == 57)
 			{continue;
 			}
 			putchar('.');
 			putchar(' ');
 	}
 	putchar('\n');
-		return (0);
+	return (0);
 }
-
-
