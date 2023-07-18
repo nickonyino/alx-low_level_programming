55214659fc73371633fac74e1643c04bc512c0a2
nickonyino <bahatiluiz@gmail.com>
2023-07-18 04:35:11 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,13 @@
  */
 void print_alphabet(void);
 {
-	int i;
-       	
-	for (i == 97; i <= 122; i++)
-	{
-		_putchar(i)
-	}
-	_putchar('\n')
+int i;
+{
+for (i == 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
 }
