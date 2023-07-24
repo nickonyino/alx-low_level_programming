87fb01ea08b56cebb14036ec681b60a03a703e1e
nickonyino <bahatiluiz@gmail.com>
2023-07-24 10:11:00 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,17 +2,11 @@
 
 /*main - function that take point to int
  * and updates value to 98
+ * @n: An integer input pointer
  * Return : 0
  */
 
 void reset_to_98(int *n);
 {
-	int n;
-
-	{
-		_putchar("n = %d\n", n);
-		reset_to_98(&n);
-		_putchar("n = %d\n", n);
-		return (0);
-	}
+	*n = 98;
 }
