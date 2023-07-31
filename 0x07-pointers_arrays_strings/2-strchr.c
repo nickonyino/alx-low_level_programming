4aadf9e8991ea2fccf5fde2c1bc9da5e192fe908
nickonyino <bahatiluiz@gmail.com>
2023-07-31 08:40:02 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include  "main.h"
+
+/**
+ * function that locates a character in a string
+ *
+ * @s: src strings
+ * @c: tested char
+ *
+ * Return: New string
+ */
+
+char *_strchr(char *s, char c);
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (0);
+}
