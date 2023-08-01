813e385e7d0c6f881455e6d2f180e02fb2ff1213
nickonyino <bahatiluiz@gmail.com>
2023-07-31 23:22:31 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,18 +5,21 @@
  * @s: input string to search in
  * @accept: set of bytes to search for
  *
- * Return: pointer to the first occurrence that matches one of the bytes in accept, or NULL if no match is found
+ * Return: pointer to the first occurrence that matches one of the
+ * bytes in accept, or NULL if no match is found
  */
 char *_strpbrk(char *s, char *accept)
 {
 	while (*s)
+
 	{
 		char *ptr = accept;
+
 		while (*ptr)
 		{
 			if (*s == *ptr)
 			{
-				return s;
+				return (s);
 			}
 			ptr++;
 		}
