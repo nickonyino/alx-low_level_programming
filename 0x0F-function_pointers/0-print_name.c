8232e1ad24b0efb4de27a21630abffcb86c2067f
nickonyino <bahatiluiz@gmail.com>
2023-08-16 05:45:32 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ *@name: string to add
+ *@f: pointer to fucntion
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if(name == NULL || f == NULL)
+		return;
+	f(name);
+}
